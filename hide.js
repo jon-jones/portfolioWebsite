e1034806c3cf3f87ba3c553e204ebad4a4c280bb
Jon Jones <jongetshismail@gmail.com>
2019-07-14 14:56:14 -0400

    hides typed text

--- hide.js
+++ hide.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $(".navbar-toggler").click(function() {
         if (toggle == false) {
             $(".hide").hide();
+            $("#typed").css("margin-top", "20%");
             toggle = true;
         } else {
             $(".hide").show();
